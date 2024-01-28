0b6d49fad9a17b7d46ca202411ddf2f1a9b9ba32
broot5 <29516943+broot5@users.noreply.github.com>
2024-01-28 21:16:32 +0900

    Split source code

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,11 @@
+mod utils;
+
 use base64::prelude::*;
 use dioxus::prelude::*;
-use log::LevelFilter;
-use qrcode::QrCode;
-use serde::Deserialize;
-use serde_json::Value;
-use std::collections::HashMap;
-use std::io::Cursor;
-use std::net::Ipv4Addr;
 
-//const URL: &str = "https://api.nordvpn.com/v1/servers?&limit=99999";
+use utils::*;
+
 const URL: &str = "https://corsproxy.io/?https://api.nordvpn.com/v1/servers?&limit=99999";
-
-#[derive(Clone, Deserialize)]
-struct Server {
-    #[allow(dead_code)]
-    id: usize,
-    #[allow(dead_code)]
-    name: String,
-    station: Ipv4Addr,
-    hostname: String,
-    load: u8,
-    status: String,
-    locations: Vec<Value>,
-    technologies: Vec<Value>,
-    groups: Vec<HashMap<String, Value>>,
-}
-
-impl Server {
-    fn country(&self) -> &str {
-        self.locations[0]["country"]["name"].as_str().unwrap()
-    }
-    fn country_code(&self) -> &str {
-        self.locations[0]["country"]["code"].as_str().unwrap()
-    }
-    fn city(&self) -> &str {
-        self.locations[0]["country"]["city"]["name"]
-            .as_str()
-            .unwrap()
-    }
-    fn public_key(&self) -> &str {
-        self.technologies[5]["metadata"][0]["value"]
-            .as_str()
-            .unwrap()
-    }
-    fn is_p2p(&self) -> bool {
-        self.groups[1].get("id").unwrap() == 15
-    }
-    fn is_wireguard(&self) -> bool {
-        self.technologies.get(5).is_some()
-    }
-}
 
 struct Input {
     private_key: String,
@@ -61,7 +17,7 @@ struct Input {
 }
 
 fn main() {
-    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    dioxus_logger::init(log::LevelFilter::Info).expect("failed to init logger");
     dioxus_web::launch(App);
 }
 
@@ -83,8 +39,8 @@ fn App(cx: Scope) -> Element {
     let p2p = use_state(cx, || true);
     let dns = use_state(cx, || String::from("1.1.1.1"));
     let textarea = use_state(cx, String::new);
-    let qrcode = use_state(cx, String::new);
-    let file_name = use_state(cx, String::new);
+    let qrcode_bytes = use_state(cx, Vec::new);
+    let server_identifier = use_state(cx, String::new);
 
     let input = Input {
         private_key: private_key.to_string(),
@@ -96,7 +52,7 @@ fn App(cx: Scope) -> Element {
     };
 
     render!(
-        h1 { "nord-wg-gen" }
+        h1 { a { href: "https://github.com/broot5/nord-wg-gen", "nord-wg-gen" } }
         div {
             label { r#for: "private_key", "Private Key" }
             input {
@@ -172,8 +128,8 @@ fn App(cx: Scope) -> Element {
                             if let Some(server) = filter_servers(&input, r) {
                                 let config = generate_config(&input, &server);
                                 textarea.set(config.clone());
-                                qrcode.set(make_qrcode(&config));
-                                file_name.set(server.hostname);
+                                qrcode_bytes.set(make_qrcode(&config));
+                                server_identifier.set(server.identifier());
                             } else {
                                 textarea
                                     .set(
@@ -195,74 +151,15 @@ fn App(cx: Scope) -> Element {
         div {
             a {
                 href: "data:text/plain;base64,{base64::engine::general_purpose::STANDARD.encode(textarea.get())}",
-                download: "{file_name}.conf",
+                download: "nord-{server_identifier}.conf",
                 button { "Download" }
             }
         }
-        div { img { alt: "QR code", src: "data:image/png;base64,{qrcode}" } }
+        div {
+            img {
+                alt: "QR Code",
+                src: "data:image/png;base64,{base64::engine::general_purpose::STANDARD.encode(qrcode_bytes.get())}"
+            }
+        }
     )
-}
-
-fn filter_servers(input: &Input, servers: &[Server]) -> Option<Server> {
-    let mut servers = servers.to_owned();
-
-    servers.retain(|x| x.is_wireguard());
-
-    servers.retain(|x| x.status == "online");
-
-    if !input.country.is_empty() {
-        servers.retain(|x| x.country() == input.country);
-    }
-
-    if !input.country_code.is_empty() {
-        servers.retain(|x| x.country_code() == input.country_code);
-    }
-
-    if !input.city.is_empty() {
-        servers.retain(|x| x.city() == input.city);
-    }
-
-    servers.retain(|x| x.is_p2p() == input.p2p);
-
-    servers.sort_by(|a, b| a.load.cmp(&b.load));
-
-    match servers.is_empty() {
-        true => None,
-        false => Some(servers[0].clone()),
-    }
-}
-
-fn generate_config(input: &Input, server: &Server) -> String {
-    format!(
-        "# Configuration for {0} ({1}) - {2}, {3}
-[Interface]
-Address = 10.5.0.2/32
-PrivateKey = {5}
-DNS = {6}
-
-[Peer]
-PublicKey = {4}
-AllowedIPs = 0.0.0.0/0
-Endpoint = {0}:51820",
-        server.hostname,
-        server.station,
-        server.city(),
-        server.country(),
-        server.public_key(),
-        input.private_key,
-        input.dns,
-    )
-}
-
-fn make_qrcode(config: &String) -> String {
-    let code = QrCode::new(config).unwrap();
-    let image = code.render::<image::Luma<u8>>().build();
-
-    let mut bytes: Vec<u8> = Vec::new();
-
-    image
-        .write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png)
-        .unwrap();
-
-    base64::engine::general_purpose::STANDARD.encode(&bytes)
 }
---
+++ src/utils.rs
@@ -0,0 +1,120 @@
+use qrcode::QrCode;
+use serde::Deserialize;
+use serde_json::Value;
+use std::collections::HashMap;
+use std::io::Cursor;
+use std::net::Ipv4Addr;
+
+use crate::Input;
+
+#[derive(Clone, Deserialize)]
+pub struct Server {
+    #[allow(dead_code)]
+    id: usize,
+    #[allow(dead_code)]
+    name: String,
+    station: Ipv4Addr,
+    hostname: String,
+    load: u8,
+    status: String,
+    locations: Vec<Value>,
+    technologies: Vec<Value>,
+    groups: Vec<HashMap<String, Value>>,
+}
+
+impl Server {
+    fn country(&self) -> &str {
+        self.locations[0]["country"]["name"].as_str().unwrap()
+    }
+    fn country_code(&self) -> &str {
+        self.locations[0]["country"]["code"].as_str().unwrap()
+    }
+    fn city(&self) -> &str {
+        self.locations[0]["country"]["city"]["name"]
+            .as_str()
+            .unwrap()
+    }
+    fn public_key(&self) -> &str {
+        self.technologies[5]["metadata"][0]["value"]
+            .as_str()
+            .unwrap()
+    }
+    fn is_p2p(&self) -> bool {
+        self.groups[1].get("id").unwrap() == 15
+    }
+    fn is_wireguard(&self) -> bool {
+        self.technologies.get(5).is_some()
+    }
+    pub fn identifier(&self) -> String {
+        self.hostname
+            .split('.')
+            .collect::<Vec<&str>>()
+            .first()
+            .unwrap()
+            .to_string()
+    }
+}
+
+pub fn filter_servers(input: &Input, servers: &[Server]) -> Option<Server> {
+    let mut servers = servers.to_owned();
+
+    servers.retain(|x| x.is_wireguard());
+
+    servers.retain(|x| x.status == "online");
+
+    if !input.country.is_empty() {
+        servers.retain(|x| x.country() == input.country);
+    }
+
+    if !input.country_code.is_empty() {
+        servers.retain(|x| x.country_code() == input.country_code);
+    }
+
+    if !input.city.is_empty() {
+        servers.retain(|x| x.city() == input.city);
+    }
+
+    servers.retain(|x| x.is_p2p() == input.p2p);
+
+    servers.sort_by(|a, b| a.load.cmp(&b.load));
+
+    match servers.is_empty() {
+        true => None,
+        false => Some(servers[0].clone()),
+    }
+}
+
+pub fn generate_config(input: &Input, server: &Server) -> String {
+    format!(
+        "# Configuration for {0} ({1}) - {2}, {3}
+[Interface]
+Address = 10.5.0.2/32
+PrivateKey = {5}
+DNS = {6}
+
+[Peer]
+PublicKey = {4}
+AllowedIPs = 0.0.0.0/0
+Endpoint = {0}:51820",
+        server.hostname,
+        server.station,
+        server.city(),
+        server.country(),
+        server.public_key(),
+        input.private_key,
+        input.dns,
+    )
+}
+
+pub fn make_qrcode(config: &String) -> Vec<u8> {
+    let code = QrCode::new(config).unwrap();
+    let image = code.render::<image::Luma<u8>>().build();
+
+    let mut bytes: Vec<u8> = Vec::new();
+
+    image
+        .write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png)
+        .unwrap();
+
+    bytes
+}
