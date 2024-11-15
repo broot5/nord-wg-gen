9cd76b1de0c501e00b0bdf9c4db4cafd845beb95
broot5 <29516943+broot5@users.noreply.github.com>
2024-11-15 22:57:16 +0000

    Deploying to gh-pages from @ broot5/nord-wg-gen@6806e5e407a5dc108e52367ce3c442cd92c6d124 🚀

--- assets/dioxus/nord-wg-gen.js
+++ assets/dioxus/nord-wg-gen.js
@@ -235,13 +235,13 @@ function __wbg_adapter_53(arg0, arg1) {
 function __wbg_adapter_56(arg0, arg1, arg2) {
     _assertNum(arg0);
     _assertNum(arg1);
-    wasm.closure625_externref_shim(arg0, arg1, arg2);
+    wasm.closure1157_externref_shim(arg0, arg1, arg2);
 }
 
 function __wbg_adapter_59(arg0, arg1, arg2) {
     _assertNum(arg0);
     _assertNum(arg1);
-    wasm.closure762_externref_shim(arg0, arg1, arg2);
+    wasm.closure1294_externref_shim(arg0, arg1, arg2);
 }
 
 function addToExternrefTable0(obj) {
@@ -1456,20 +1456,20 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper1339 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 501, __wbg_adapter_50);
+    imports.wbg.__wbindgen_closure_wrapper3600 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1033, __wbg_adapter_50);
         return ret;
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper1491 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 622, __wbg_adapter_53);
+    imports.wbg.__wbindgen_closure_wrapper3752 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1154, __wbg_adapter_53);
         return ret;
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper1493 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 622, __wbg_adapter_56);
+    imports.wbg.__wbindgen_closure_wrapper3754 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1154, __wbg_adapter_56);
         return ret;
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper1772 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 763, __wbg_adapter_59);
+    imports.wbg.__wbindgen_closure_wrapper4033 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1295, __wbg_adapter_59);
         return ret;
     }, arguments) };
     imports.wbg.__wbindgen_init_externref_table = function() {
